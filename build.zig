a021577fd671f6741b7d2a3044546d6697449ff6
Francis <fq@francisqureshi.com>
2025-12-28 01:22:26 +0000

    cbuild.zig

--- build.zig
+++ build.zig
@@ -3,9 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
+    const optimize = b.standardOptimizeOption(.{});
     const mod = b.addModule("smpte", .{
         .root_source_file = b.path("src/smpte.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const mod_tests = b.addTest(.{
